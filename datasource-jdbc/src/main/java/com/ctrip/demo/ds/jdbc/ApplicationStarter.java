6e90f9b2a973265810bcabeabc3ea1fa2e468e09
liuhuanjie <huanjieliu@trip.com>
2023-08-08 15:01:52 +0800

    init dal

--- datasource-jdbc/src/main/java/com/ctrip/demo/ds/jdbc/ApplicationStarter.java
+++ datasource-jdbc/src/main/java/com/ctrip/demo/ds/jdbc/ApplicationStarter.java
@@ -1,12 +1,13 @@
-package com.ctrip.demo;
+package com.ctrip.demo.ds.jdbc;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ApplicationStarter {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ApplicationStarter.class, args);
     }
 }
---
+++ datasource-jdbc/src/main/java/com/ctrip/demo/ds/jdbc/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.ctrip.demo.ds.jdbc.controller;
+
+
+import com.ctrip.datasource.configure.DalDataSourceFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    private DalDataSourceFactory factory = new DalDataSourceFactory();
+
+    @GetMapping(value = "/get/datasource")
+    public String getDatasource() throws Exception {
+        DataSource dbaDatasource = factory.getOrCreateDataSource("dbadalclustertest01db_dalcluster");
+        try (Connection connection = dbaDatasource.getConnection()) {
+            return connection.getMetaData().getURL();
+        }catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+}
